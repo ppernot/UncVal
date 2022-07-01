a47d23f06164e3755aa8237cce8707656934c3e0
Pascal PERNOT <pascal.pernot@u-psud.fr>
2022-07-01 12:14:13 +0200

    test

--- R/packages.R
+++ R/packages.R
@@ -8,33 +8,35 @@ options(
 
 # Local install ----
 
-# CRAN Libraries ----
-libs <- c(
-  "devtools", "boot",
-  "shiny", "shinyFiles","shinycssloaders",
-  "data.table"
-
-)
-for (lib in libs) {
-  if (!require(lib, character.only = TRUE, quietly = TRUE))
-    install.packages( lib,
-      # repos = "https://cran.univ-paris1.fr",
-      dependencies = FALSE
-    )
-  library(lib, character.only = TRUE, quietly = TRUE)
-}
-# ## Other libraries ----
+# # CRAN Libraries ----
+# libs <- c(
+#   "devtools", "boot",
+#   "shiny", "shinyFiles","shinycssloaders",
+#   "data.table"
+#
+# )
+# for (lib in libs) {
+#   if (!require(lib, character.only = TRUE, quietly = TRUE))
+#     install.packages( lib,
+#       # repos = "https://cran.univ-paris1.fr",
+#       dependencies = FALSE
+#     )
+#   library(lib, character.only = TRUE, quietly = TRUE)
+# }
+# # ## Other libraries ----
 lib = "ErrViewLib"
 if(!require(lib,character.only = TRUE))
-devtools::install_github(paste0("ppernot/",lib))
+devtools::install_github(
+  paste0("ppernot/",lib),
+  dependencies = TRUE)
 library(lib,character.only = TRUE)
 
 # Cloud deployment ----
-#
-# library("devtools")
-# library("shiny")
-# library("shinyFiles")
-# library("shinycssloaders")
-# library("boot")
+
+library("devtools")
+library("shiny")
+library("shinyFiles")
+library("shinycssloaders")
+library("boot")
 # library("ErrViewLib")
-# library("data.table")
+library("data.table")
